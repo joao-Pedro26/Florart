da4504f95f091a6e89012e282d49a45f907c93a1
João <joaopedroferreiradasilva454@gmail.com>
2025-09-11 11:31:06 -0300

    ajuste

--- js/cabecalho.js
+++ js/cabecalho.js
@@ -1,9 +1,7 @@
 
 document.addEventListener('DOMContentLoaded', function() {
-    // Detecta se está fora da home
     const isHome = window.location.pathname.endsWith('home.php') || window.location.pathname === '/' || window.location.pathname === '/index.php';
 
-    // Links do menu
     const homeLink = document.getElementById('home-menu');
     const catalogoLink = document.getElementById('catalogo-menu');
 
