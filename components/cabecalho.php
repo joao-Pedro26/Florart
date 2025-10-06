869c52e0a269dbfb086d6c97c18a372576ef089a
joaoPedro26 <joao.pf.silva@unesp.br>
2025-10-06 20:22:42 -0300

    Começo da alteração no cabaçalho (Colocar o icon de perfil)

--- components/cabecalho.php
+++ components/cabecalho.php
@@ -1,11 +1,9 @@
-
-
 <header class="cabecalho">
     <a href="/"><img src="../images/logo.png" alt="logo" class="img-logo"></a>
-  
+
     <input type="checkbox" id="check">
     <label for="check" class="icons">
-        <i class='bx bx-menu' id="icon-menu"></i> 
+        <i class='bx bx-menu' id="icon-menu"></i>
         <i class='bx bxs-x' id="icon-fechar"></i>
     </label>
     <nav class="menu">
@@ -19,12 +17,12 @@
         if (!isset($_SESSION['statusLogado']) || $_SESSION['statusLogado'] !== true) {
             echo '<a href="../pages/loginTeste.php">Login</a>';
             echo '<a href="../pages/cadastroTeste.php">Cadastro</a>';
-        } 
+        }
         // Se for ADMIN
         elseif (isset($_SESSION['admin']) && $_SESSION['admin'] === true) {
             echo '<a href="../pages/admin.php">Central de Controle</a>';
             echo '<a href="../pages/home.php?route=acoes/sair">Sair</a>';
-        } 
+        }
         // Se for USUÁRIO comum
         else {
             echo '<a href="../pages/perfil.php">Perfil</a>';
@@ -32,21 +30,27 @@
         }
         ?>
     </nav>
-             
 
-        <div class="icon-carrinho">
-            <button>
-                <i class="bx bx-cart" id="icon-carrinho" style="color:#ffffff"></i>
-            </button>
-        </div>
-         <script> 
-                const iconCarrinho = document.querySelector('.icon-carrinho');
-               
-                <?php if(basename($_SERVER['PHP_SELF']) === 'finaliza-compra.php'): ?>
-                    iconCarrinho.style.display = 'none';
-                <?php endif; ?>
-        </script>
+
+    <div class="icon-carrinho-perfil">
+        <button>
+            <i class="bx bx-cart icon-carrinho-perfil" id="icon-carrinho-perfil" style="color:#ffffff"></i>
+        </button>
+        <?php 
+        if (isset($_SESSION['statusLogado']) && $_SESSION['statusLogado'] === true){
+        echo "<button>
+        <i class='bxd bx-user icon-carrinho-perfil' id='icon-carrinho-perfil' style='color:#ffffff'></i>
+        </button>";
+        }
+        ?>
+    </div>
+    <script>
+        const iconCarrinho = document.querySelector('.icon-carrinho');
+
+        <?php if (basename($_SERVER['PHP_SELF']) === 'finaliza-compra.php'): ?>
+            iconCarrinho.style.display = 'none';
+        <?php endif; ?>
+    </script>
 </header>
 
- <script src="../js/cabecalho.js"></script>
- 
+<script src="../js/cabecalho.js"></script>
--- model/produtosModel.php
+++ model/produtosModel.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 class ProdutosModel extends Database {
 
