0be50780b85179196e2c17fb7b59340a8baad0af
joaoPedro26 <joao.pf.silva@unesp.br>
2025-10-17 20:29:47 -0300

    Adicao do meus pedidos

--- pages/perfil.php
+++ pages/perfil.php
@@ -25,30 +25,57 @@ if (!isset($_SESSION['statusLogado']) || $_SESSION['statusLogado'] !== true) {
     <!-- Cabeçalho -->
     <?php include '../components/cabecalho.php'; ?>
     <main class="main-perfil">
-    <a href="javascript:history.back()"><i class='bxr  bx-arrow-left-stroke icon-voltar' style='color:#5a2ff4'></i> </a>
-    <a href="#"><img src="/images/logo.png" alt="logo" class="logo-log-cad logo-cad"></a>
+        <a href="javascript:history.back()"><i class='bxr  bx-arrow-left-stroke icon-voltar' style='color:#5a2ff4'></i> </a>
+        <img src="/images/logo.png" alt="logo" class="logo-log-cad logo-cad">
 
         <section class="perfil">
-                <h2>Meu Perfil</h2>
-                <div class="dados-usuario">
-                    <div class="icon-editar">
-                <a href="/pages/editar-perfil.php"><i class='bxr  bx-edit icon-editar'  style='color:#000000'></i> </a>
+            <h2>Meu Perfil</h2>
+            <div class="dados-usuario">
+                <div class="icon-editar">
+                    <a href="/pages/editar-perfil.php"><i class='bxr  bx-edit icon-editar' style='color:#000000'></i> </a>
                 </div>
-                    <label >Email:</label>
-                    <div class="campo">
-                        <p>joao@gmail.com</p>
-                    </div>
-                    <label >Nome:</label>
-                    <div class="campo">
-                        <p>Joao</p>
-                    </div>
-                     <label >Telefone:</label>
-                    <div class="campo">
-                        <p>14997973331  </p>
-                    </div>
+                <label>Email:</label>
+                <div class="campo">
+                    <p>joao@gmail.com</p>
+                </div>
+                <label>Nome:</label>
+                <div class="campo">
+                    <p>Joao</p>
+                </div>
+                <label>Telefone:</label>
+                <div class="campo">
+                    <p>14997973331 </p>
+                </div>
 
-                </div>
+            </div>
         </section>
+            <section class="perfil" >
+                <h2>Meus Pedidos</h2>
+            
+                    <div class="produto">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Pedido Nº</th>
+                                    <th>Data</th>
+                                    <th>Quantidade</th>
+                                    <th>Valor Total</th>
+                                    <th>Status</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Flor #2232</td>
+                                    <td>34-56-3455</td>
+                                    <td>34535</td>
+                                    <td>35345,36</td>
+                                    <td>Cancelado</td>
+                                </tr>
+                            </tbody>
+
+                        </table>     
+                </div>
+            </section>
     </main>
     <?php include '../components/menu-mobile.php'; ?>
 
